1dd63bd8e75fa0902868f26be28728cf1abc3f44
Adam Ross Benton <adamrossbenton@gmail.com>
2021-07-29 10:43:09 -0700

    trying appendChild

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ const numberOfInvFields = e => {
             addMid.placeholder = "Middle";
         const addLast = document.createElement('INPUT');
             addLast.placeholder = "Last";
-        document.querySelector('body').append(addFirst);
-        document.querySelector('body').append(addMid);
-        document.querySelector('body').append(addLast);
+        document.querySelector('body').appendChild(addFirst);
+        document.querySelector('body').appendChild(addMid);
+        document.querySelector('body').appendChild(addLast);
     }
 }
 
