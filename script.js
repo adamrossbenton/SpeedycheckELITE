31ef20c06fb1b0a7bbdaa6cbcf96006fae3c72e0
Adam Ross Benton <adamrossbenton@gmail.com>
2021-07-28 23:30:48 -0700

    add mid & last (css will need fix)

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 const numberOfInvFields = e => {
     let invNumber = e.target.value;
     for (let n = 0; n < invNumber; n++) {
-        const addFields = document.createElement('INPUT');
-            addFields.placeholder = "First";
-        document.querySelector('body').append(addFields);
+        const addFirst = document.createElement('INPUT');
+            addFirst.placeholder = "First";
+        const addMid = document.createElement('INPUT');
+            addMid.placeholder = "Middle";
+        const addLast = document.createElement('INPUT');
+            addLast.placeholder = "Last";
+        document.querySelector('body').append(addFirst);
+        document.querySelector('body').append(addMid);
+        document.querySelector('body').append(addLast);
     }
 }
 
