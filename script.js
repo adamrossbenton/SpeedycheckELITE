692d583cfcf14ca344d64addc5c10df0b6be116e
Adam Ross Benton <adamrossbenton@gmail.com>
2021-07-28 22:26:30 -0700

    add class & remove old p

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //Function to add number of investigator name fields (first, middle, last, suffix) based on button pushed
 const numberOfInvFields = e => {
     let invNumber = e.target.value;
+    const el = document.getElementsByClassName('newField');
+        el.remove();
     const addFields = document.createElement('p');
         addFields.innerText = `${invNumber}`;
+        addFields.className = 'newField'
     document.body.append(addFields)
     /*let i = 1;
     while (i < invNumber) {
