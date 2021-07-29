661d10eba8060ca64a41bccea1523ae8fb30c850
Adam Ross Benton <adamrossbenton@gmail.com>
2021-07-28 23:42:31 -0700

    CSS for 3 fields.3 (remove add text)

--- script.js
+++ script.js
@@ -2,15 +2,12 @@
 const numberOfInvFields = e => {
     let invNumber = e.target.value;
     for (let n = 0; n < invNumber; n++) {
-        const addText = document.createElement('p');
-            addText.innerText = `Inv no. ${n+1}`;
         const addFirst = document.createElement('INPUT');
             addFirst.placeholder = "First";
         const addMid = document.createElement('INPUT');
             addMid.placeholder = "Middle";
         const addLast = document.createElement('INPUT');
             addLast.placeholder = "Last";
-        document.querySelector('body').append(addText);
         document.querySelector('body').append(addFirst);
         document.querySelector('body').append(addMid);
         document.querySelector('body').append(addLast);
