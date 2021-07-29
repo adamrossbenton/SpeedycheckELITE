37c6d3ef6f50cbd815bfe8b008d762915a0fcd26
Adam Ross Benton <adamrossbenton@gmail.com>
2021-07-28 22:06:00 -0700

    testing some js...

---
+++ script.js
@@ -0,0 +1,12 @@
+//Function to add number of investigator name fields (first, middle, last, suffix) based on button pushed
+const numberOfInvFields = e => {
+    let invNumber = e.target.value;
+    console.log(invNumber);
+    /*let i = 1;
+    while (i < invNumber) {
+
+    }*/
+}
+
+//Make the buttons do stuff
+document.querySelector('button').addEventListener('click',numberOfInvFields)
