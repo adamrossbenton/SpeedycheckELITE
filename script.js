ca127b92f4a83e58f8f94126fd905310e8c77f74
Adam Ross Benton <adamrossbenton@gmail.com>
2021-07-28 23:36:08 -0700

    CSS for 3 fields.1

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 const numberOfInvFields = e => {
     let invNumber = e.target.value;
     for (let n = 0; n < invNumber; n++) {
+        const addText = document.createElement('p');
+            addText.innerText = `Inv no. ${n}`;
         const addFirst = document.createElement('INPUT');
             addFirst.placeholder = "First";
         const addMid = document.createElement('INPUT');
             addMid.placeholder = "Middle";
         const addLast = document.createElement('INPUT');
             addLast.placeholder = "Last";
+        document.querySelector('body').append(addText);
         document.querySelector('body').append(addFirst);
         document.querySelector('body').append(addMid);
         document.querySelector('body').append(addLast);
