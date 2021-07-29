2edca768fe27f327e687c79785e9e5e09fd80002
Adam Ross Benton <adamrossbenton@gmail.com>
2021-07-29 10:09:16 -0700

    center everything again.1 (re-add addText

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //Function to add number of investigator name fields (first, middle, last, suffix) based on button pushed
 const numberOfInvFields = e => {
     let invNumber = e.target.value;
+    const addText = document.createElement('p');
+        addText.innerText = "Enter the investigator names in the fields below:";
     for (let n = 0; n < invNumber; n++) {
         const addFirst = document.createElement('INPUT');
             addFirst.placeholder = "First";
